1f9ad57e734e82447d476795878cf5aa346e6285
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-12-21 12:39:04 +0300

    Add files via upload

---
+++ DubDubGrub/DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Maxim Bekmetov on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
