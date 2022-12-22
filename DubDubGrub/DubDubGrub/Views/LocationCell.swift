86356a6fd516a8fecd16f867912c9d78aa8b3be9
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-12-22 18:03:01 +0300

    refactoring

---
+++ DubDubGrub/DubDubGrub/Views/LocationCell.swift
@@ -0,0 +1,45 @@
+//
+//  LocationCell.swift
+//  DubDubGrub
+//
+//  Created by Maxim Bekmetov on 22.12.2022.
+//
+
+import SwiftUI
+
+struct LocationCell: View {
+    
+    var body: some View {
+        HStack {
+            Image("default-square-asset")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+                .padding(.vertical, 8)
+            
+            VStack(alignment: .leading) {
+                Text("Test Location Name")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+                
+                HStack {
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                }
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+struct LocationCell_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationCell()
+    }
+}
---
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationListView.swift
@@ -0,0 +1,29 @@
+//
+//  LocationListView.swift
+//  DubDubGrub
+//
+//  Created by Maxim Bekmetov on 21.12.2022.
+//
+
+import SwiftUI
+
+struct LocationListView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(0..<10) { item in
+                    NavigationLink(destination: LocationDetailView()) {
+                        LocationCell()
+                    }
+                }
+            }
+            .navigationTitle("Grub Spots")
+        }
+    }
+}
+
+struct LocationListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationListView()
+    }
+}
