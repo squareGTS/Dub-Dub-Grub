6644317f02fb1a1dfb545319c7cd324919b87fe7
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-12-23 18:43:27 +0300

    add user defaults

--- DubDubGrub/DubDubGrub/Views/Screens/LocationMapView/LocationMapView.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationMapView/LocationMapView.swift
@@ -27,11 +27,14 @@ struct LocationMapView: View {
                 Spacer()
             }
         }
+        .sheet(isPresented: $viewModel.isShowingOnboardView, onDismiss: viewModel.checkIfLocationServicesIsEnabled) {
+            OnboardView(isShowingOnboardView: $viewModel.isShowingOnboardView)
+        }
         .alert(item: $viewModel.alertItem, content: { alertItem in
             Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
         })
         .onAppear{
-            viewModel.checkIfLocationSericeIsEnabled()
+            viewModel.runStartupChecks()
             if locationManager.locations.isEmpty {
                 viewModel.getLocations(for: locationManager)
             }
--- DubDubGrub/DubDubGrub/Views/Screens/OnboardView.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/OnboardView.swift
@@ -8,15 +8,18 @@
 import SwiftUI
 
 struct OnboardView: View {
+    @Binding var isShowingOnboardView: Bool
+    
     var body: some View {
         VStack {
             HStack {
                 Spacer()
                 Button {
-                    
+                    isShowingOnboardView = false
                 } label: {
                     XDismissButton()
                 }
+                .padding()
             }
             Spacer()
             
@@ -45,7 +48,7 @@ struct OnboardView: View {
 
 struct OnboardView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardView()
+        OnboardView(isShowingOnboardView: .constant(true))
     }
 }
 
