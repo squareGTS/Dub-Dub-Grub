c75c8d3b04b786de1aa9393ddf3b2beb6e978735
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-12-28 18:05:52 +0300

    openMaps for directions make a call

--- DubDubGrub/DubDubGrub/Views/Screens/LocationDetailView/LocationDetailView.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationDetailView/LocationDetailView.swift
@@ -8,25 +8,22 @@
 import SwiftUI
 
 struct LocationDetailView: View {
-    let columns = [GridItem(.flexible()),
-                   GridItem(.flexible()),
-                   GridItem(.flexible())]
     
-    var location: DDGLocation
+    @ObservedObject var viewModel: LocationDetailViewModel
     
     var body: some View {
         
         VStack(spacing: 16) {
-            BannerImageView(image: location.createBannerImage())
+            BannerImageView(image: viewModel.location.createBannerImage())
             
             HStack {
-                AdressView(adress: location.address)
+                AdressView(adress: viewModel.location.address)
                 
                 Spacer()
             }
             .padding(.horizontal)
             
-            DescriptionView(text: location.description)
+            DescriptionView(text: viewModel.location.description)
             
             ZStack {
                 Capsule()
@@ -35,16 +32,16 @@ struct LocationDetailView: View {
                 
                 HStack(spacing: 20) {
                     Button {
-                        
+                        viewModel.getDirectionsToLocation()
                     } label: {
                         LocationActionButton(color: .brandPrimary, imageName: "location.fill")
                     }
-                    Link(destination: URL(string: location.websiteURL)!, label: {
+                    Link(destination: URL(string: viewModel.location.websiteURL)!, label: {
                         LocationActionButton(color: .brandPrimary, imageName: "network")
                     })
                     
                     Button {
-                        
+                        viewModel.callLocation()
                     } label: {
                         LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
                     }
@@ -62,7 +59,7 @@ struct LocationDetailView: View {
                 .font(.title2)
             
             ScrollView {
-                LazyVGrid(columns: columns, content: {
+                LazyVGrid(columns: viewModel.columns, content: {
                     FirstNameAvstsrName(image: PlaceholderImage.avatar, firstName: "Sean")
                     FirstNameAvstsrName(image: PlaceholderImage.avatar, firstName: "Sean")
                     FirstNameAvstsrName(image: PlaceholderImage.avatar, firstName: "Sean")
@@ -74,7 +71,10 @@ struct LocationDetailView: View {
             }
             Spacer()
         }
-        .navigationTitle(location.name)
+        .alert(item: $viewModel.alertItem, content: { alertItem in
+            Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+        })
+        .navigationTitle(viewModel.location.name)
         .navigationBarTitleDisplayMode(.inline)
     }
 }
@@ -82,7 +82,7 @@ struct LocationDetailView: View {
 struct LocationDetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            LocationDetailView(location: DDGLocation(record: MockData.location))
+            LocationDetailView(viewModel: LocationDetailViewModel(location: DDGLocation(record: MockData.location)))
         }
     }
 }
---
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationDetailView/LocationDetailViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  LocationDetailViewModel.swift
+//  DubDubGrub
+//
+//  Created by Maxim Bekmetov on 28.12.2022.
+//
+
+import SwiftUI
+import MapKit
+
+final class LocationDetailViewModel: ObservableObject {
+    
+    @Published var alertItem: AlertItem?
+    
+    let columns = [GridItem(.flexible()),
+                   GridItem(.flexible()),
+                   GridItem(.flexible())]
+    
+    var location: DDGLocation
+    
+    init(location: DDGLocation) {
+        self.location = location
+    }
+    
+    func getDirectionsToLocation() {
+        let placemark = MKPlacemark(coordinate: location.location.coordinate)
+        let mapItem = MKMapItem(placemark: placemark)
+        
+        mapItem.name = location.name
+        
+        mapItem.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeWalking])
+        
+    }
+    
+    func callLocation() {
+        guard let url = URL(string: "tel://\(location.phoneNumber)") else {
+            alertItem = AlertContext.invalidPhoneNumber
+            return
+        }
+        UIApplication.shared.open(url)
+    }
+}
