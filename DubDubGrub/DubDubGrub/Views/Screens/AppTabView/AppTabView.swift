add414989716a2068f46ca59879ab4f15e7a72f4
Maxim <68609996+squareGTS@users.noreply.github.com>
2023-01-10 12:43:20 +0300

    custom map annotation implementation

--- DubDubGrub/DubDubGrub/Views/Screens/AppTabView/AppTabView.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/AppTabView/AppTabView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct AppTabView: View {
+    @StateObject private var viewModel = AppTabViewModel()
+    
     var body: some View {
         TabView {
             LocationMapView()
@@ -25,8 +27,15 @@ struct AppTabView: View {
                 Label("Profile", systemImage: "person")
             }
         }
-        .onAppear { CloudKitManager.shared.getUserRecord() }
+        .onAppear {
+            CloudKitManager.shared.getUserRecord()
+            viewModel.runStartupChecks()
+            
+        }
         .accentColor(.brandPrimary)
+        .sheet(isPresented: $viewModel.isShowingOnboardView, onDismiss: viewModel.checkIfLocationServicesIsEnabled) {
+            OnboardView(isShowingOnboardView: $viewModel.isShowingOnboardView)
+        }
     }
 }
 
--- DubDubGrub/DubDubGrub/Views/Screens/LocationMapView/LocationMapViewModel.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationMapView/LocationMapViewModel.swift
@@ -7,53 +7,11 @@
 
 import MapKit
 
-final class LocationMapViewModel: NSObject, ObservableObject {
-    @Published var isShowingOnboardView = false
+final class LocationMapViewModel: ObservableObject {
+    @Published var isShowingDetailView = false
     @Published var alertItem: AlertItem?
     @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-    
-    var deviceLocationManager: CLLocationManager?
-    let kHasSeenOnboardView = "hasSeenOnboardView"
-    
-    var hasSeenOnboardView: Bool {
-        return UserDefaults.standard.bool(forKey: kHasSeenOnboardView)
-    }
-    
-    func runStartupChecks() {
-        if !hasSeenOnboardView {
-            isShowingOnboardView = true
-            UserDefaults.standard.set(true, forKey: kHasSeenOnboardView)
-        } else {
-            checkIfLocationServicesIsEnabled()
-        }
-    }
-    
-    func checkIfLocationServicesIsEnabled() {
-        if CLLocationManager.locationServicesEnabled() {
-            deviceLocationManager = CLLocationManager()
-            deviceLocationManager!.delegate = self
-        } else {
-            alertItem = AlertContext.locationDisabled
-        }
-    }
-    
-    func checkLocationAuthorization() {
-        guard let deviceLocationManager = deviceLocationManager else { return }
-        
-        switch deviceLocationManager.authorizationStatus {
-        case .notDetermined:
-            deviceLocationManager.requestWhenInUseAuthorization()
-        case .restricted:
-            alertItem = AlertContext.locationRestricted
-        case .denied:
-            alertItem = AlertContext.locationDenied
-        case .authorizedAlways, .authorizedWhenInUse:
-            break
-        @unknown default:
-            break
-        }
-    }
-    
+ 
     func getLocations(for locationManager: LocationManager) {
         CloudKitManager.shared.getLocations { result in
             DispatchQueue.main.async {
@@ -65,11 +23,5 @@ final class LocationMapViewModel: NSObject, ObservableObject {
                 }
             }
         }
-    }
-}
-
-extension LocationMapViewModel: CLLocationManagerDelegate {
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        checkLocationAuthorization()
     }
 }
