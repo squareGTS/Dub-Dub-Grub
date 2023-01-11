9c756e56ec8d9f5ad226277e0db66a6c5ddb6b0c
Maxim <68609996+squareGTS@users.noreply.github.com>
2023-01-11 22:07:07 +0300

    refactoring ios 15

--- DubDubGrub/DubDubGrub/Views/Screens/LocationListView/LocationListView.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationListView/LocationListView.swift
@@ -24,6 +24,7 @@ struct LocationListView: View {
                 }
             }
             .navigationTitle("Grub Spots")
+            .listStyle(.plain)
             .onAppear { viewModel.getCheckedInProfilesDictionary() }
             .alert(item: $viewModel.alertItem) { $0.alert }
         }
